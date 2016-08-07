1b45c1e28a1706b7989b22d2ffbcbdf64e375aaf
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-07 14:26:00 -0700

    Create a01-generic-installs.sh

---
+++ a01-generic-installs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#assumes both these commands have been done
+#sudo su
+#apt-get update
+
+
+
+#lots of basic developer pakages. Not a big deal if we try to install them again.
+
+sudo apt-get -y install pkg-config g++ zlib1g-dev unzip zip python-pip python-dev python-virtualenv libblas-dev liblapack-dev libatlas-base-dev gfortran
