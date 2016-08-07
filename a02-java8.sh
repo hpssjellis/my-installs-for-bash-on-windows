ca057e071ef6656fd616f3bd0e67dbc5ed3d9f5d
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-07 14:38:28 -0700

    Update a02-java8.sh

--- a02-java8.sh
+++ a02-java8.sh
@@ -4,6 +4,8 @@
 #sudo su
 #apt-get update
 
+cd ~/
+
 
 wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-linux-x64.tar.gz
 
@@ -12,6 +14,15 @@ mkdir /opt/jdk
 tar -zxf jdk-8u5-linux-x64.tar.gz -C /opt/jdk
 
 ls /opt/jdk
+
+
+export JAVA_HOME=/opt/jdk/jdk1.8.0_05
+export PATH=$PATH:$JAVA_HOME/bin
+
+printf "\n\nexport JAVA_HOME=/opt/jdk/jdk1.8.0_05\nexport PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc 
+
+
+
 
 
 #The last number 1100 is the priority mine had java 7 at 1097 online help site had these were set at 100
