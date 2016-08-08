714d462101fdf84033e0746335588f72d21b65c6
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-07 18:19:42 -0700

    Update a04-android.sh

--- a04-android.sh
+++ a04-android.sh
@@ -30,3 +30,21 @@ export PATH=$PATH:$ANDROID_SDK_HOME/platform-tools
 
 echo "Android sdk installed now you have more to do."
 echo "This works best using bash from the power shell"
+
+
+
+
+echo "y" | android update sdk --filter platform-tools --no-ui --force 
+echo "y" | android update sdk --filter tools --no-ui --force
+
+echo "y" | android update sdk --filter extra --no-ui --force
+
+echo "y" | android update sdk --filter extra --no-ui --force
+echo "y" | android update sdk --all --filter build-tools-23.0.2 --no-ui --force
+echo "y" | android update sdk --filter android-20 --no-ui --force   #Android 4.4W.2 KitKat
+#echo "y" | android update sdk --filter android-22 --no-ui --force   #Android 5.1.1  Lollipop
+#echo "y" | android update sdk --filter android-23 --no-ui --force   #Android 6.0    Marshmallow
+
+
+
+
