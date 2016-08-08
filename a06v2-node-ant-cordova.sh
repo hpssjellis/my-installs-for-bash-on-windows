f40ff64dc7d3a8b0dec771dcc586084cfcf9a69f
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-08 08:21:12 -0700

    Update a06v2-node-ant-cordova.sh

--- a06v2-node-ant-cordova.sh
+++ a06v2-node-ant-cordova.sh
@@ -4,45 +4,27 @@
 #sudo su
 #apt-get update
 
-sudo apt-get -y install node
-sudo apt-get -y install npm
+sudo apt-get -y install node npm ant
 
 
 #npm install -g phonegap@latest
 npm install -g cordova
 
-sudo apt-get install -y ant
-
-#installing android a different way
-
-
-#sudo apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
-
-#mkdir /home/android
-
-#cd /home/android
-
-#note use this url to get the android sdk https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
-
-#wget https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
-
-#sudo tar -zxvf /home/android/android-sdk_r24.0.2-linux.tgz -C /home/android/
-
-#export PATH=$PATH:/home/android/android-sdk-linux/
-#export PATH=$PATH:/home/android/android-sdk-linux/tools
-#export PATH=$PATH:/home/android/android-sdk-linux/platform-tools
-#export PATH=$PATH:/home/android/android-sdk-linux/build-tools
-
 echo "Check versions"
-git --version
-#android -h
-phonegap -v
-ant -V
+
+cordova -v
 node -v
 npm -v
+ant -V
+#note the weird capital V for ant?
 
+
+Echo "Node is sometimes hard to find to make paths to"
 whereis node
 which node
+
+whereis cordova
+which cordova
 
 echo "Now lets test phonegap cordova"
 
@@ -51,7 +33,7 @@ mkdir /home/cordova
 cd /home/cordova
 
 #get out of sudo
-exit
+#exit
 
 
 #note: phonegap and cordova are kind of the same thing. cordova is opensource, phonegap is Adobes
