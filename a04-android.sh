6e7d82ea1a17b5b0e928ac3f38db30ee689c4ebc
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-07 17:51:44 -0700

    Create a04-android.sh

---
+++ a04-android.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#assumes both these commands have been done
+#sudo su
+#apt-get update
+
+
+mkdir /home/android
+
+cd /home/android
+
+
+sudo wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz -O /home/android/android-sdk_r24.4.1-linux.tgz
+sudo tar -xvzf /home/android/workspace/android-sdk_r24.4.1-linux.tgz -C /home/android/
+#sudo rm android-sdk_r24.4.1-linux.tgz
+
+
+
+echo "export paths to the .profile file so other terminals can use android sdk"
+
+printf "\n\nexport ANDROID_SDK_HOME=/home/android/android-sdk-linux\nexport PATH=\$PATH:\$ANDROID_SDK_HOME/tools\nexport PATH=\$PATH:\$ANDROID_SDK_HOME/platform-tools"  >> ~/.bashrc
+
+echo "export paths here so this bash file can use android sdk"
+export ANDROID_SDK_HOME=/home/android/android-sdk-linux
+export PATH=$PATH:$ANDROID_SDK_HOME/tools
+export PATH=$PATH:$ANDROID_SDK_HOME/platform-tools
+
+
+
+
+echo "Android sdk installed now you have more to do."
+echo "try typing android sdk"
