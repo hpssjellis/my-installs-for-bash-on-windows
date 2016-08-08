084aac557727fcf4db9135a5fd1e7c679364c845
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-08 07:12:46 -0700

    Update a03v2-bazel0.3.1.sh

--- a03v2-bazel0.3.1.sh
+++ a03v2-bazel0.3.1.sh
@@ -13,11 +13,9 @@
 sudo mkdir /home/bazel
 cd /home/bazel
 
-sudo wget https://github.com/bazelbuild/bazel/releases/download/0.3.1/bazel-0.3.1-installer-linux-x86_64.sh -O ~/mymagenta/bazel/bazel-0.3.1-installer-linux-x86_64.sh
+sudo wget https://github.com/bazelbuild/bazel/releases/download/0.3.1/bazel-0.3.1-installer-linux-x86_64.sh
 
 sudo chmod +x bazel-0.3.1-installer-linux-x86_64.sh
-
-echo "Presently these next two commands not working well. May have to do from the command line"
 
 sudo ./bazel-0.3.1-installer-linux-x86_64.sh --user
 #bash bazel-0.2.3-installer-linux-x86_64.sh --user --bin=${IDEDIR}/bin
@@ -26,26 +24,20 @@ sudo ./bazel-0.3.1-installer-linux-x86_64.sh --user
 export BAZEL_HOME=/home/bazel
 export PATH="$PATH:$BAZEL_HOME/bin"
 
+#sudo rm bazel-0.3.1-installer-linux-x86_64.sh
+printf "\n\nexport BAZEL_HOME=/home/bazel\nexport PATH=\$PATH:\$BAZEL_HOME/bin" >> ~/.bashrc 
+#printf "\n\nsource ./bin/bazel-complete.bash"  >> ~/.bashrc
 
 
 echo "Just test if bazel is working"
-#sudo rm bazel-0.3.1-installer-linux-x86_64.sh
-printf "\n\nexport BAZEL_HOME=/home/bazel\nexport PATH=\$PATH:\$BAZEL_HOME/bin" >> ~/.bashrc 
 
 bazel
-
-
 
 bazel -h
 
 echo "Might be needed if you open a new terminal to reset the path to bazel"
 
-#printf "\n\nsource ./bin/bazel-complete.bash"  >> ~/.profile
 echo ""
 echo "Did bazel install? Should see the bazel help info"
 
 
-
-
-
-cd ~/mymagenta
