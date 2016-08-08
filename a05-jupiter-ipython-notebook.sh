e5a6fc20a9ec2e4ed68b9ba0a44779df7b6b73d3
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-08 08:59:39 -0700

    Update a05-jupiter-ipython-notebook.sh

--- a05-jupiter-ipython-notebook.sh
+++ a05-jupiter-ipython-notebook.sh
@@ -13,10 +13,13 @@ pip install --upgrade ipython
 pip install --upgrade jupyter
 
 
-#Do all your other commands above this line
+#Do all your other commands above this line as the terminal will stay working on the last line
 echo "On windows onpe a web browser to http://localhost:8080"
+echo "No more commands for this terminal"
+echo "ctrl c then quickly y to quit"
 
-jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
 #jupyter notebook --ip $IP --port $PORT --no-browser
+jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
 
-# bash file stays here so no more commands will work
+
+
