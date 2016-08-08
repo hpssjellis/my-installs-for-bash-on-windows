6281a288f543bb749febc6eedb6f8b96a293bb80
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-08 08:57:48 -0700

    Update a05-jupiter-ipython-notebook.sh

--- a05-jupiter-ipython-notebook.sh
+++ a05-jupiter-ipython-notebook.sh
@@ -14,7 +14,7 @@ pip install --upgrade jupyter
 
 
 #Do all your other commands above this line
-
+echo "On windows onpe a web browser to http://localhost:8080"
 
 jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
 #jupyter notebook --ip $IP --port $PORT --no-browser
