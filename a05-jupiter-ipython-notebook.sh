005b29b27e462414b4982ac9d8610970fd889579
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-08 08:50:07 -0700

    Create a05-jupiter-ipython-notebook.sh

---
+++ a05-jupiter-ipython-notebook.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#assumes both these commands have been done
+#sudo su
+#apt-get update
+
+
+echo "---------------------Try jupiter ipython notebook-----------------------------------------"
+echo ". "
+
+
+pip install --upgrade ipython
+pip install --upgrade jupyter
+
+
+#Do all your other commands above this line
+
+
+jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
+#jupyter notebook --ip $IP --port $PORT --no-browser
+
+# bash file stays here so no more commands will work
