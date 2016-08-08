0493009b55ef40c1691156883c811ca606546101
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-07 19:43:44 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ apt-get update
 
 #I prefer to use bash then-a-script.sh but most people make the script runnable with chmod
 chmod +x a01-generic-installs.sh
-chmod +x a02-java8.sh
+#chmod +x a02-java8.sh
 #chmod +x a0.sh
 #chmod +x a0.sh
 #chmod +x a0.sh
@@ -24,11 +24,12 @@ chmod +x a02-java8.sh
 ./a01-generic-installs.sh
 
 #install java 8
-./a02-java8.sh
+#./a02-java8.sh
 
 
 
 
 echo "Only the proven working scripts are in this script"
 echo "You can load the other scripts to see if you can get them working"
+echo "Remember to either use chmod +x and ./filename or just bash then the file name"
 echo "Done installing all scripts"
