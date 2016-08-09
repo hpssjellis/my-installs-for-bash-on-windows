96162a0e291b8bc82bde60e9b39084719643b1b4
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-09 12:57:40 -0700

    Update a03v2-bazel0.3.1.sh

--- a03v2-bazel0.3.1.sh
+++ a03v2-bazel0.3.1.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
+
+mkdir /home/$USER/bazel
+cd /home/$USER/bazel
+
+
+
 # has a 5 minute constrain as sudo su
 sudo su <<RUNNING_AS_SUPER_SU
 echo "as me $USER and $MY_USER but running as sudo su"
 
-
-sudo mkdir /home/$USER/bazel
 cd /home/$USER/bazel
 
-#sudo wget https://github.com/bazelbuild/bazel/releases/download/0.3.1/bazel-0.3.1-installer-linux-x86_64.sh -O ~/mymagenta/bazel/bazel-0.3.1-installer-linux-x86_64.sh
-sudo wget https://github.com/bazelbuild/bazel/releases/download/0.3.1/bazel-0.3.1-installer-linux-x86_64.sh
 
-sudo chmod +x bazel-0.3.1-installer-linux-x86_64.sh
+wget https://github.com/bazelbuild/bazel/releases/download/0.3.1/bazel-0.3.1-installer-linux-x86_64.sh -O /home/$USER/bazel/bazel-0.3.1-installer-linux-x86_64.sh
+#wget https://github.com/bazelbuild/bazel/releases/download/0.3.1/bazel-0.3.1-installer-linux-x86_64.sh
+
+chmod +x bazel-0.3.1-installer-linux-x86_64.sh
 
 echo "Presently these next two commands not working well. May have to do from the command line"
 
-sudo ./bazel-0.3.1-installer-linux-x86_64.sh --user
+./bazel-0.3.1-installer-linux-x86_64.sh --user
+
+
+# done running as supper user
 
 RUNNING_AS_SUPER_SU
+
+
+
+
+
+
 
 #bash bazel-0.2.3-installer-linux-x86_64.sh --user --bin=${IDEDIR}/bin
 
