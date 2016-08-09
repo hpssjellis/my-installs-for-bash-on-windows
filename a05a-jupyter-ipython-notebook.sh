1833e8b1a05e63daecffc20d6d57d8deb7fb681b
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-09 00:34:07 -0700

    Update and rename a05-jupiter-ipython-notebook.sh to a05a-jupyter-ipython-notebook.sh

---
+++ a05a-jupyter-ipython-notebook.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "The following must be used to re-activate virtual environment for tensorflow"
+echo "source ~/virtual-tf/bin/activate"
+source /home/$USER/virtual-tf/bin/activate 
+
+
+echo "---------------------Try jupiter ipython notebook-----------------------------------------"
+echo ". "
+
+
+pip install --upgrade ipython
+pip install --upgrade jupyter
+
+
+echo "This just installs jupyter. You must run it"
+
+
+
