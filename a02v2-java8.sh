e796f3182fb77edb8df56023aff4bb41e03834a3
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-09 00:24:18 -0700

    Update a02v2-java8.sh

--- a02v2-java8.sh
+++ a02v2-java8.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-#assumes both these commands have been done
-#sudo su
-#apt-get update
 
-mkdir /home/jdk
-cd /home/jdk
+
+mkdir /home/$USER/jdk
+cd /home/$USER/jdk
 
 wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-linux-x64.tar.gz
 
-tar -zxf jdk-8u5-linux-x64.tar.gz -C /home/jdk
+tar -zxf jdk-8u5-linux-x64.tar.gz -C /home/$USER/jdk
 
-cd /home/jdk/jdk1.8.0_05
+cd /home/$USER/jdk/jdk1.8.0_05
 
 ls
 
 echo "Jave 8 installed. Now setting paths for this bash session and others to follow."
 
 #exports so that this bash instance runs well
-export JAVA_HOME=/home/jdk/jdk1.8.0_05
+export JAVA_HOME=/home/$USER/jdk/jdk1.8.0_05
 export PATH=$PATH:$JAVA_HOME/bin
 
 #printf so that a new instance (window) using bash will still run.
-printf "\n\nexport JAVA_HOME=/home/jdk/jdk1.8.0_05\nexport PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc 
+printf "\n\nexport JAVA_HOME=/home/\$USER/jdk/jdk1.8.0_05\nexport PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc 
 
 
 
@@ -30,9 +28,9 @@ printf "\n\nexport JAVA_HOME=/home/jdk/jdk1.8.0_05\nexport PATH=\$PATH:\$JAVA_HO
 
 #The last number 1100 is the priority mine had java 7 at 1097 online help site had these were set at 100
 
-update-alternatives --install /usr/bin/java java /home/jdk/jdk1.8.0_05/bin/java 1100
+update-alternatives --install /usr/bin/java java /home/$USER/jdk/jdk1.8.0_05/bin/java 1100
 
-update-alternatives --install /usr/bin/javac javac /home/jdk/jdk1.8.0_05/bin/javac 1100
+update-alternatives --install /usr/bin/javac javac /home/$USER/jdk/jdk1.8.0_05/bin/javac 1100
 
 update-alternatives --display java
 
