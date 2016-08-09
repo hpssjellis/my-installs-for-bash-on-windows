effd24d73a16c47b36d75b22967adfb0735c2aef
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-09 08:57:58 -0700

    Update test-setup.sh

--- test-setup.sh
+++ test-setup.sh
@@ -1,29 +1,31 @@
 #!bin/bash
 
 
-a01-generic-installs.sh
+bash a01-generic-installs.sh
 
-a02v2-java8.sh
+bash a02v2-java8.sh
 
-a03v2-bazel0.3.1.sh
+bash a03v2-bazel0.3.1.sh
 
 
 #running in virtual
 
-a04a-tensorflow.sh
-a04b-run-virtual
-a04c-tf-test.py
-a04d-skflow.sh
-a05a-jupyter-ipython-notebook.sh
-a07-magenta.sh
+bash a04a-tensorflow.sh
+bash a04b-run-virtual
 
+bash a04d-skflow.sh
+bash a05a-jupyter-ipython-notebook.sh
+bash a07-magenta.sh
 
+# out of virtual environment
 deactivate
 
-a06v2-node-ant-cordova.sh
+bash a08-switch-java-1.7.sh
 
+bash a06v2-node-ant-cordova.sh
 
-a10-android.sh
+bash a10-android.sh
 
-a11v2-gradle3.0rc2.sh
+# do gradle later
+#a11v2-gradle3.0rc2.sh
 
