4f41e5826cd504a3b89bb9b6a0d10d392b5c88d1
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-10 06:58:29 -0700

    Update a11v3-gradle.sh

--- a11v3-gradle.sh
+++ a11v3-gradle.sh
@@ -2,9 +2,24 @@
 
 
 
+# has a 5 minute constrain as sudo su but with your home directory
+echo "Running as sudo su but in the home folder of $USER"
+sudo su <<UNTIL_STOP_RUNNING_AS_SUDO_SU
 
-sudo apt-get install -y gradle
+  cd /home/$USER
 
-echo "gradle installed. What about paths?"
+  sudo apt-get install -y gradle
 
+UNTIL_STOP_RUNNING_AS_SUDO_SU
+
+
+echo "Now checking if Gradle is installed"
 gradle -v
+echo ""
+echo "echo "Now checking where gradle is installed"
+readlink -f $(which gradle)
+echo "--------------------------------------------------------------"
+echo ". "
+
+
+cd /home/$USER
