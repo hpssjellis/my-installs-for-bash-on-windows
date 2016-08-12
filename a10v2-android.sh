fb985dc9e9fdabecfea2036e76ffe47eafd99ed8
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-11 19:31:56 -0700

    Update a10v2-android.sh

--- a10v2-android.sh
+++ a10v2-android.sh
@@ -41,22 +41,22 @@ echo "probably best not to do the echo y |"
 #echo "y" | android update sdk --filter "platform-tools,tools,extra,build-tools-23.0.2,android-20,android-22,android-23" --no-ui --force 
 
 
-echo "installing android"
+echo "installing android sdk update"
 # has a 5 minute constrain as sudo su but with your home directory
 echo "Running as sudo su but in the home folder of $USER"
 sudo su <<DONE_AS_SUPER_SU
 
 
-echo "y" | android update sdk --filter platform-tools --no-ui --force 
-echo "y" | android update sdk --filter tools --no-ui --force
-echo "y" | android update sdk --filter extra --no-ui --force
+android update sdk --filter platform-tools --no-ui --force 
+android update sdk --filter tools --no-ui --force
+android update sdk --filter extra --no-ui --force
 
-echo "y" | android update sdk --filter extra --no-ui --force
-echo "y" | android update sdk --all --filter build-tools-23.0.2 --no-ui --force
-echo "y" | android update sdk --filter android-20 --no-ui --force   #Android 4.4W.2 KitKat
+android update sdk --filter extra --no-ui --force
+android update sdk --all --filter build-tools-23.0.2 --no-ui --force
+android update sdk --filter android-20 --no-ui --force   #Android 4.4W.2 KitKat
 
-#echo "y" | android update sdk --filter android-22 --no-ui --force   #Android 5.1.1  Lollipop
-#echo "y" | android update sdk --filter android-23 --no-ui --force   #Android 6.0    Marshmallow
+#android update sdk --filter android-22 --no-ui --force   #Android 5.1.1  Lollipop
+#android update sdk --filter android-23 --no-ui --force   #Android 6.0    Marshmallow
 
 
 DONE_AS_SUPER_SU
