6104149bffe1f6064a01c9aa6ba01830ae401f65
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-12 09:21:07 -0700

    Update a91b-all-checks-save-errors.sh

--- a91b-all-checks-save-errors.sh
+++ a91b-all-checks-save-errors.sh
@@ -5,6 +5,6 @@
 #run the checks twice
 #open a new terminal and run the following commands again
 
-bash a91-all-checks.sh 2>&1 | tee a091-all-output.txt
+bash a91-all-checks.sh 2>&1 | tee z-all-output.txt
 
-bash a91-all-checks.sh 2> a091-errors-only.txt
+bash a91-all-checks.sh 2> z-errors-only.txt
