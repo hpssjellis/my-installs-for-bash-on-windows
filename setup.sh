ef3757601735e45ee7707e0f7695c6b2c8501320
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-11 20:31:31 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,38 @@
 #!bin/bash
 
 
+#for errors to output file and terminal and rewrite old file
+#bash a91-all-checks.sh 2>&1 | tee log.txt
+
+#if you have errors then write 
+#bash a91-all-checks.sh 2>log5.txt
+
+
+#To view    search (/ text to search Enter) in the file; press q to exit.
+#less log5.txt
+
+
+
+
+
+
+
 bash a01-generic-installs.sh
 
-bash a10v2-android.sh
+
+
+# not really the java I want but it seems to work. Oracles is giving me problems
+bash a02v2-java8.sh
 
 bash a06v2-node-ant-cordova.sh
 
 bash a11v3-gradle.sh
 
-bash a02v2-java8.sh
 
-bash a03v2-bazel0.3.1.sh
+#open a new terminal and try the checks again to see if the paths have been set coorectly 
+# in the ~/.bashrc file
+
+
 
 
 #running in virtual
@@ -32,13 +53,26 @@ bash a07-magenta.sh
 
 
 
-bash a10-android.sh
+#bash a10-android.sh
+
+#issues with input at oracle statement
+#bash a02v3-java8.sh 
+
+#bash a02v2-java8.sh
 
 
 
 
-bash a91-all-checks.sh
+
+bash a10v2-android.sh
 
 
+bash a03v2-bazel0.3.1.sh
+
+
+#run the checks twice so that errors can be trapped in its own fle
+bash a91-all-checks.sh 2>&1 | tee a091-all-output.txt
+
+bash a91-all-checks.sh 2> a091-errors-only.txt
 
 
