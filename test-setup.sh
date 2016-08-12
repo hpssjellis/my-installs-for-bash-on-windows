85fdd17ce5e559cd3901e9026f7e2c9d3a054f9c
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-12 07:46:30 -0700

    Update test-setup.sh

--- test-setup.sh
+++ test-setup.sh
@@ -32,21 +32,21 @@
 #!/bin/bash
 
 echo "apt-get update"
-sudo apt-get update
+#sudo apt-get update
 
-#lots of basic developer pakages. Not a big deal if we try to install them again.
+#lots of basic developer packages. Not a big deal if we try to install them again.
 
 echo "Installing generic helpers"
 
 
 sudo su <<RUNNING_AS_SUPER_SU
-cd /home/$USER/jdk
+cd /home/$USER
 
-apt-get install -y git-core gnupg flex bison gperf build-essential \
+apt-get install -y update git-core gnupg flex bison gperf build-essential \
   zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
   lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
   libgl1-mesa-dev libxml2-utils xsltproc unzip lib32z1 git pkg-config \
-  g++ unzip python-pip python-dev python-virtualenv  \
+  g++ python-pip python-dev python-virtualenv  \
   libblas-dev liblapack-dev libatlas-base-dev gfortran openjdk-7-jdk
   
 RUNNING_AS_SUPER_SU 
