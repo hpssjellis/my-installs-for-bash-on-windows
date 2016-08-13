d7da338f9008a5c8ee798bab1eea2ace4e919775
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-13 08:01:34 -0700

    Update a02v3-java8oracle.sh

--- a02v3-java8oracle.sh
+++ a02v3-java8oracle.sh
@@ -32,6 +32,9 @@ sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 
+#following can setup the environment variables
+#sudo apt-get install oracle-java8-set-default
+
 
 
 
