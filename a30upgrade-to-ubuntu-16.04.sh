0ff9a69492c2b920cbc28669ae407888737c4cf6
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-14 21:20:44 -0700

    Create a30upgrade-to-ubuntu-16.04.sh

---
+++ a30upgrade-to-ubuntu-16.04.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+
+
+echo "try to upgrade to ubuntu 16.04"
+
+echo "Your current release is"
+lsb_release -a
+
+
+
+echo "you will have to ctrl-c out of this first step"
+
+
+sudo do-release-upgrade -f DistUpgradeViewNonInteractive -d
+
+ sudo dpkg --configure -a
+
+sudo apt-get update
+sudo apt-get dist-upgrade 
+sudo apt-get autoremove
+
+echo "Did that work to install ubuntu 16.04??"
+
+lsb_release -a
+
+
+
+
+
