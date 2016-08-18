79555ecb7b3e6ca9534fadd0c39fe87db420a101
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-17 17:29:19 -0700

    Su

--- a30-upgrade-to-ubuntu-16.04.sh
+++ a30-upgrade-to-ubuntu-16.04.sh
@@ -9,18 +9,18 @@ lsb_release -a
 
 
 
-echo "you will have to ctrl-c out of this first step"
+#echo "you will have to ctrl-c out of this first step"
 
 
-sudo do-release-upgrade -f DistUpgradeViewNonInteractive -d
+#sudo do-release-upgrade -f DistUpgradeViewNonInteractive -d
 
- sudo dpkg --configure -a
+ #sudo dpkg --configure -a
 
-sudo apt-get update
-sudo apt-get dist-upgrade 
-sudo apt-get autoremove
+#sudo apt-get update
+#sudo apt-get dist-upgrade 
+#sudo apt-get autoremove
 
-echo "Did that work to install ubuntu 16.04??"
+#echo "Did that work to install ubuntu 16.04??"
 
 lsb_release -a
 
@@ -30,15 +30,22 @@ lsb_release -a
 #someone else got this working
 
 
+sudo su <<RUNNING_AS_SUPER_SU
 
-#sudo su
-#apt-get update
-#apt-get upgrade
-#apt-get dist-upgrade
-#apt-get autoremove
-#sudo do-release-upgrade -f DistUpgradeViewNonInteractive -d
+cd /home/$USER
 
 
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+apt-get autoremove
+do-release-upgrade -f DistUpgradeViewNonInteractive -d
 
 
+RUNNING_AS_SUPER_SU
 
+
+lsb_release -a
+
+
+echo "Did it work"
