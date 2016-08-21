467fa7441728543ef94a3d73621a87e5636b7eec
Jeremy Ellis <keyfreemusic@gmail.com>
2016-08-20 23:31:25 -0700

    Update a02v3-java8oracle.sh

--- a02v3-java8oracle.sh
+++ a02v3-java8oracle.sh
@@ -24,13 +24,20 @@
 echo "Installing oracle java 8 with silent license acceptance"
 
 
+#sudo su <<RUNNING_AS_SUPER_SU
+#cd /home/$USER
 
-
-sudo apt-get install -y python-software-properties debconf-utils
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
+apt-get install -y python-software-properties debconf-utils
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-installer
+
+
+
+RUNNING_AS_SUPER_SU 
+
+
 
 #following can setup the environment variables
 #sudo apt-get install oracle-java8-set-default
